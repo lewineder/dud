a401571eb77632aec9b35f0cf7dfa78b7eda3089
leoniefauser <leonifauser@gmail.com>
2022-03-29 17:25:52 +0200

    update main.scala added scailing and scroll

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,14 +1,37 @@
-@main
-def field(arg1: Int  = 4, arg2: Int = 4, arg3: Int = 6, arg4: Int = 3): Unit =
+@main 
+def fieldUser(cellsx: Int  = 4, cellsy: Int = 4, cellw: Int = 6, cellh: Int = 3): Unit =
   println("Entry")
-  println(cells(arg1, arg2, arg3, arg4))
+  println(cells(cellsx, cellsy, cellw, cellh))
 
-val eol: String = sys.props("line.separator")
+@main
+def field(): Unit =
+  println("Entry")
+  println(cells(8,5,5,2))
 
-def gencell(cellw: Int = 4, cellh: Int = 2, cellsx: Int = 3): String =
-  ((" +" + ("-" * cellw) + "+") * cellsx + eol) +
-    ((" |" + (" " * cellw) + "|") * cellsx + eol) * cellh +
-    ((" +" + ("-" * cellw) + "+") * cellsx)
+val eol = sys.props("line.separator")
+val sep = "    "
 
-def cells(cellsx: Int = 8, cellsy: Int = 5, cellw: Int = 5, cellh: Int = 2): String =
-  (gencell(cellw, cellh, cellsx) + eol) * cellsy
+def genscrollent(cellw: Int = 4, cellh: Int = 2, cellsx: Int = 3) =
+  val con = cellw*cellsx + cellsx *3
+  (sep +  ("_" * (con + 8)) + eol +
+  (" / \\ " + sep + sep + " " * con + "\\." + eol) +
+  ("|   |" + sep + sep + (" " * con) + "|." + eol) +
+  (" \\_ |" + sep + sep + (" " * con) + "|." + eol))
+
+
+def genscrollend(cellw: Int, cellsx: Int) = 
+  val con = cellw*cellsx + cellsx *3
+  (sep + "|" + "   " + ("_" * con) + "_____|___" + eol) +
+  ( sep + "|" + "  /" + (" " * con) + "        /" + eol) +
+  ( sep + "\\" + "_/_" + ("_" * con) + "_______/ " + eol) 
+
+def gencell(cellw: Int = 4, cellh: Int = 2, cellsx: Int = 3) =
+  (sep + "|" + sep + ( " +" + ("-" * cellw) + "+") * cellsx + sep + "|. " + eol) +
+  (sep + "|" + sep + ( " |" + (" " * cellw) + "|") * cellsx + sep + "|. " + eol) * cellh +
+  (sep + "|" + sep + ( " +" + ("-" * cellw) + "+") * cellsx + sep + "|. " )
+
+def cells(cellsx: Int = 8, cellsy: Int = 5, cellw: Int = 5, cellh: Int = 2) =
+  genscrollent(cellw, cellh, cellsx) +
+  (gencell(cellw, cellh, cellsx) + eol) * cellsy +
+  genscrollend(cellw, cellsx)
+
