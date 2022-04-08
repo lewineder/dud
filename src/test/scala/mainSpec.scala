ec05750a356c372f4f22861c8b41458181df5979
leder <lewin.eder@htwg-konstanz.de>
2022-04-08 08:59:08 +0200

    Added Jacoco-Test in plugins.sbt + fixed some Tests for field-functions

--- src/test/scala/mainSpec.scala
+++ src/test/scala/mainSpec.scala
@@ -1,32 +1,41 @@
 import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers.*
 
-class mainSpec extends wordspec.AnyWordSpec {
+class mainSpec extends AnyWordSpec {
   "main" should {
+    "Have a main 'field' with a Unit-return" in {
+      field(1, 1, 5, 2) should be (())
+
+    }
+    /**
     "have a Greeting-Message-String of form 'Drunter und Drüber'" +
       "and have a method field creating a String of form\n" +
       "+-----+\n|     |\n|     |\n+-----+" in {
       field(1, 1, 5, 2) should be
         ("Drunter und Drüber" + eol +
           " +-----+" + eol + " |     |" + eol + " |     |" + eol + " +-----+" + eol)
+    */
+    "have a value Line-Seperator eol" in {
+      eol should be (sys.props("line.separator"))
     }
+
     "have a scalable field in x and y-Direction" in {
-      genCellsY(1, 1, 1, 1) should be (" +-+" + eol + " | |" + eol + " +-+")
+      genCellsY(1, 1, 1, 1) should be (" +-+" + eol + " | |" + eol + " +-+" + eol)
       genCellsY(2, 2, 2, 2) should be
         (" +--+ +--+" + eol + " |  | |  |" + eol + " |  | |  |" + eol + " +--+ +--+" + eol +
         " +--+ +--+" + eol + " |  | |  |" + eol + " |  | |  |" + eol + " +--+ +--+" + eol)
     }
     "have a scalable row of scalable Cells like " +
     "+-----+ +-----+\n|     | |     |\n|     | |     |\n+-----+ +-----+" in {
-      genCellsX(1, 1, 1) should be (" +-+" + eol + " | |" + eol + " +-+")
+      genCellsX(1, 1, 1) should be (" +-+" + eol + " | |" + eol + " +-+" + eol)
       genCellsX(2, 2, 2) should be
-        (" +--+ +--+" + eol + " |  | |  |" + eol + " |  | |  |" + eol + " +--+ +--+")
+        (" +--+ +--+" + eol + " |  | |  |" + eol + " |  | |  |" + eol + " +--+ +--+" + eol)
 
     }
     "have a scalable Tops '+-+'" in {
-      genCellTop(0) should be(" ++" + eol)
-      genCellTop(1) should be(" +-+" + eol)
-      genCellTop(2) should be(" +--+" + eol)
+      genCellTop(0) should be(" ++")
+      genCellTop(1) should be(" +-+")
+      genCellTop(2) should be(" +--+")
     }
     "have scalable Walls '|'" in {
       genCellBody(0) should be(" ||")
@@ -34,9 +43,9 @@ class mainSpec extends wordspec.AnyWordSpec {
       genCellBody(2) should be(" |  |")
     }
     "have a scalable Bottom" in {
-      genCellBottom(0) should be(" ++" + eol)
-      genCellBottom(1) should be(" +-+" + eol)
-      genCellBottom(2) should be(" +--+" + eol)
+      genCellBottom(0) should be(" ++")
+      genCellBottom(1) should be(" +-+")
+      genCellBottom(2) should be(" +--+")
     }
   }
 }
