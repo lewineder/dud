b7b94d29be4fcb02a3f288bcb259341d7bce8673
lewineder <lewin.eder@htwg-konstanz.de>
2022-05-11 16:44:21 +0200

    Added more Testing-Lines and updated scala.yml

--- src/test/scala/dud/aview/TuiSpec.scala
+++ src/test/scala/dud/aview/TuiSpec.scala
@@ -13,5 +13,13 @@ class TuiSpec extends AnyWordSpec {
     "recognize the input 11 S1 as switch of an empty field to field (S1)" in {
       tui.getInputLine("11 S1") should be(Some(Move(1, 1, Building.S1)))
     }
+    "and following lines:" in {
+      tui.getInputLine("11 S2") should be(Some(Move(1, 1, Building.S2)))
+      tui.getInputLine("11 S3") should be(Some(Move(1, 1, Building.S3)))
+      tui.getInputLine("11 H1") should be(Some(Move(1, 1, Building.H1)))
+      tui.getInputLine("11 H2") should be(Some(Move(1, 1, Building.H2)))
+      tui.getInputLine("11 H3") should be(Some(Move(1, 1, Building.H3)))
+      tui.getInputLine("11 H4") should be(Some(Move(1, 1, Building.H4)))
+    }
   }
 }
