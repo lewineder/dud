dedc0b0da71a20585ca53eb3daedf6ac732c694b
lewineder <98407013+lewineder@users.noreply.github.com>
2022-06-24 15:26:29 +0200

    Changes in testing

--- src/test/scala/dud/model/GameSpec.scala
+++ src/test/scala/dud/model/GameSpec.scala
@@ -10,23 +10,24 @@ import util.*
 class GameSpec extends AnyWordSpec {
     "A game " when {
 
-        val field1 = new Field(1, 1, Building("Ey"))
-        val field2 = new Field(1, 1, Building("S1"))
+        val field1 = new Field(4, 4, Building("Ey"))
+        val field2 = new Field(1, 1, Building("Ey"))
+        val field3 = new Field(1, 1, Building("S1"))
         val player = Array(Player("Farin", 1), Player("Rod", 1), Player("Bela", 1), Player("Herbert", 1))
         val turn = Turn(1)
         val game = Game(field1, player, turn)
+        val game2 = Game(field2, player, turn)
         "filled" should {
             "use the Methods" in {
 
                 game.handle(Play()).toString should be("Some(Spieler 1 Nächste Strasse setzen)")
 
-                game.toString should include ("[1] Gebäude verbleibend für Spieler << Farin >>")
-                game.toString should include ("[1] Gebäude verbleibend für Spieler << Rod >>")
-                game.toString should include ("[1] Gebäude verbleibend für Spieler << Bela >>")
-                game.toString should include ("[1] Gebäude verbleibend für Spieler << Herbert >>")
+                game.toString should include ("SpielerFarin:  Points = 0")
+                game.toString should include ("SpielerRod:  Points = 0")
+                game.toString should include ("SpielerBela:  Points = 0")
+                game.toString should include ("SpielerHerbert:  Points = 0")
 
-
-                game.setBuilding(0, 0, Building("S1")).toString should be(Game(field2, player, turn).toString)
+                game2.setBuilding(0, 0, Building("S1")).toString should be(Game(field3, player, turn).toString)
 
             }
         }
