ee5f23bd72b729cb442ebeec0c29ca92ee544490
leoniefauser <leonifauser@gmail.com>
2022-06-30 08:29:32 +0200

    add help, improve testcoverage

--- src/test/scala/dud/model/BuildingSpec.scala
+++ src/test/scala/dud/model/BuildingSpec.scala
@@ -11,6 +11,12 @@ class BuildingSpec extends AnyWordSpec {
     "!_________!\n" + "___________\n" + "|         |\n" + "+---------+\n")
   Building("Ey").toString should be ("+---------+\n" +
     "|         |\n"+ "|         |\n" + "|         |\n" + "+---------+\n")
+  Building("S2").toString should be ("+--| ¦ |--+\n" + "!__| ¦ |__!\n" + 
+  "___     ___\n" + "|  | ¦ |  |\n" + "+--| ¦ |--+\n")
+  Building("S3").toString should be ("+--| ¦ |--+\n" + "|  | ¦ |o |\n" + "|  | ¦ || |\n" + "|  | ¦ |  |\n" + "+--| ¦ |--+\n"
+)
+  Building("S4").toString should be ("+--| ¦ |--+\n" + "!__| ¦ |__!\n" + "___     ___\n" + "|  | ¦ |  |\n" + "+--| ¦ |--+\n")
+  Building("S5").toString should be ("+--| ¦ |--+\n" + "!__| ¦ |__!\n" + "___     ___\n" + "|  | ¦ |  |\n" + "+--| ¦ |--+\n")
 }
 
 
